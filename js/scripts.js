d428f54b681cc022f59376f504739a3e71136e3d
valnekesa <valentinenekesa9@gmail.com>
2018-05-07 11:04:26 +0300

    modified scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+//business logic
+function converter(input) {
+  var result = [];
+  for(var i=1; i<= input; i++){
+    if ((i % 3 === 0) && (i % 5 === 0 )){
+      $(".output").append("<li>" + "pingpong" + "</li>");
+    } else if (i % 3 === 0) {
+      $(".output").append("<li>" + "ping" + "</li>");
+    }
+    else if (i % 5 === 0) {
+      $(".output").append("<li>" + "pong" + "</li>");
+    }
+    else {
+        $(".output").append("<li>" + i + "</li>");
+      }
+    }
+  }
+
+//  user interface logic
+$(document).ready(function() {
+  $("form#numberInput").submit(function(event){
+
+    $(".output").text("");
+
+    var input = $("input#number").val();
+    converter(input);
+
+    event.preventDefault();
+    });
+
+  });
